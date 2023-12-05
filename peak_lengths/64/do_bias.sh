1bfd8d5a433cf2c7453f5e33e6771f878cea973c
Spencer James Gibson <sjgibson@lanec1.compbio.cs.cmu.edu>
2023-12-04 20:38:00 -0500

    Updated bash script to create GC matched negative sequences, construct bed files from them, and perform synteny split

--- peak_lengths/64/do_bias.sh
+++ peak_lengths/64/do_bias.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-biasaway g -f THP1_peaks.fa -r /home/sjgibson/data/ifb_background/hg_38_biasaway_background > output.fa
+biasaway g -f THP1_peaks.fa -r /home/sjgibson/data/ifb_background/hg38_background_lengths/64/hg_38_background_64 > output.fa
--- peak_lengths/create_bed_lengths.sh
+++ peak_lengths/create_bed_lengths.sh
@@ -8,17 +8,32 @@ do
         
         # Call your Python script with the directory name
         echo "Processing directory: " $dir_name
-	python /home/sjgibson/atac_data_pipeline/scripts/preprocessing.py expand_peaks -l $dir_name -i /home/sjgibson/data/THP1_monocyte/THP1_monocytes.idr.optimal_peak.narrowPeak -o /home/sjgibson/data/THP1_monocyte/peak_lengths/$dir_name/temp.bed
+	#python /home/sjgibson/atac_data_pipeline/scripts/preprocessing.py expand_peaks -l $dir_name -i /home/sjgibson/data/THP1_monocyte/CompBio/THP1_monocytes.idr.optimal_peak.narrowPeak -o /home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/temp.bed
 
-	python /home/sjgibson/atac_data_pipeline/scripts/preprocessing.py filter_length -i /home/sjgibson/data/THP1_monocyte/peak_lengths/$dir_name/temp.bed -o /home/sjgibson/data/THP1_monocyte/peak_lengths/$dir_name/THP1_peaks.bed --conditions "<=$dir_name" ">=$dir_name"
+	#python /home/sjgibson/atac_data_pipeline/scripts/preprocessing.py filter_length -i /home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/temp.bed -o /home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/THP1_peaks.bed --conditions "<=$dir_name" ">=$dir_name"
 	#python /path/to/script.py $dir_name
 	
-	rm /home/sjgibson/data/THP1_monocyte/peak_lengths/$dir_name/temp.bed
+	#rm /home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/temp.bed
 
 	# Get the positives fasta file
-	bedtools getfasta -fi /home/sjgibson/data/genomes/hg38/hg38.fa -bed /home/sjgibson/data/THP1_monocyte/peak_lengths/$dir_name/THP1_peaks.bed > /home/sjgibson/data/THP1_monocyte/peak_lengths/$dir_name/THP1_peaks.fa
+	#bedtools getfasta -fi /home/sjgibson/data/genomes/hg38/hg38.fa -bed /home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/THP1_peaks.bed > /home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/THP1_peaks.fa
     
 	# Use biasaway to get GC matched negative sequences
-	
+	#biasaway g -f /home/sjgibson/data/THP1_\
+#monocyte/CompBio/peak_lengths/$dir_name/THP1_peaks.fa -r /home/sjgibson/data/ifb_background/hg38_background_lengths/$dir_name/hg_38_background_$dir_name > /home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/THP1_negatives.fa
+
+	# Get corresponding bed files for negative sequence fasta files
+	#python fasta_to_bed.py -fi /home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/THP1_negatives.fa -bed /home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/THP1_negatives.bed -l $dir_name
+
+	# Use synteny split method to create train/test/val split with 5 folds for each directory
+	NEG=/home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/THP1_negatives.bed
+	POS=/home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/THP1_peaks.bed
+	SPLIT_DIR_NEG=/home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/neg/
+	SPLIT_DIR_POS=/home/sjgibson/data/THP1_monocyte/CompBio/peak_lengths/$dir_name/pos/
+	#mkdir $SPLIT_DIR_NEG
+	#mkdir $SPLIT_DIR_POS
+	python /home/sjgibson/atac_data_pipeline/scripts/preprocessing.py split --in_files $POS --split_ref_species hg38 --out_dir $SPLIT_DIR_POS --create_folds 1 2 3 4 5
+	python /home/sjgibson/atac_data_pipeline/scripts/preprocessing.py split --in_files $NEG --split_ref_species hg38 --out_dir $SPLIT_DIR_NEG --create_folds 1 2 3 4 5
+
     fi
 done
